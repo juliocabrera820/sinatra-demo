211ba7bfcda588b7baeeae53b1351e2b79d6438d
julio cabrera <juliocabrera820@gmail.com>
2021-07-18 00:47:17 -0500

    test sign up feature

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -12,10 +12,21 @@ RSpec.feature 'the sign up process', type: :feature do
         fill_in 'password', with: 'password'
       end
       click_button 'Crear'
+      expect(page).to have_css '.alert'
+      expect(page).to have_css '.alert-success'
       expect(page).to have_content 'Usuario agregado satisfactoriamente'
+      expect(page).to have_css '.container'
+      expect(page).to have_css '.form-group'
       expect(page).to have_content 'Correo'
+      expect(page).to have_css '.form-control'
+      expect(page).to have_field 'email'
+      expect(page).to have_css '.form-group'
       expect(page).to have_content 'Password'
-      expect(page).to have_content 'Iniciar sesión'
+      expect(page).to have_css '.form-control'
+      expect(page).to have_field 'password'
+      expect(page).to have_css '.btn'
+      expect(page).to have_css '.btn-success'
+      expect(page).to have_button 'Iniciar sesión'
     end
     scenario 'Data is invalid' do
       visit '/auth/sign_up'
@@ -25,10 +36,25 @@ RSpec.feature 'the sign up process', type: :feature do
         fill_in 'password', with: ''
       end
       click_button 'Crear'
+      expect(page).to have_css '.alert'
+      expect(page).to have_css '.alert-warning'
       expect(page).to have_content 'Error con los datos ingresados'
+      expect(page).to have_css '.container'
+      expect(page).to have_css '.form-group'
       expect(page).to have_content 'Nombre'
+      expect(page).to have_field 'name'
+      expect(page).to have_css '.form-control'
+      expect(page).to have_css '.form-group'
       expect(page).to have_content 'Correo'
+      expect(page).to have_css '.form-control'
+      expect(page).to have_field 'email'
+      expect(page).to have_css '.form-group'
       expect(page).to have_content 'Password'
+      expect(page).to have_css '.form-control'
+      expect(page).to have_field 'password'
+      expect(page).to have_css '.btn'
+      expect(page).to have_css '.btn-primary'
+      expect(page).to have_button 'Crear'
     end
   end
 end
